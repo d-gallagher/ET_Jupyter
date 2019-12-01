aad850b0240e6429efc096da686d922e181b76a5
d-gallagher <jibisjib@gmail.com>
2019-12-01 16:51:46 +0000

    Issues with Array Shape when predicting - resize w/scipy or np?

--- Flask/app.py
+++ Flask/app.py
@@ -1,7 +1,15 @@
+import tensorflow as tf
+import base64
+import io
+import numpy as np
+
 from flask import Flask, escape, request, render_template, url_for, jsonify, make_response
 from flask_cors import CORS
-import tensorflow as tf
-import uuid
+from PIL import Image, ImageFilter
+from matplotlib import pyplot as plt
+from scipy.misc import imread
+from keras.preprocessing.image import ImageDataGenerator
+from keras.preprocessing.image import img_to_array
 
 app = Flask(__name__)
 CORS(app)
@@ -33,20 +41,37 @@ def testPage():
 @app.route('/postmethod', methods = ['POST'])
 def post_javascript_data():
     # print(request.form.to_dict())
-    # Using Force - Always try and parse data as JSON
+    # Get the request with the image data (Force - Always try and parse data as JSON)
     canvasData = request.get_json(force=True)
-    print(canvasData)
+    # Get the image data from the request
+    encodedCanvasData = canvasData['data']
+    # # Decode the data from base64 image
+    decodedCanvasData = base64.b64decode(encodedCanvasData)
+    # save image as png
+    # https://stackoverflow.com/questions/2323128/convert-string-in-base64-to-image-and-save-on-filesystem-in-python
+    with open('image.png', 'wb') as im:
+        im.write(decodedCanvasData)
+    # read saved image in as grayscale
+    x = imread('output.png', mode='L')
+    # ValueError: Error when checking input: expected flatten_input to have 3 dimensions, but got array with shape (250, 300)
+    prediction = kModel.predict(x).toList() 
+
+    print("prediction")
+    print(prediction)
+
     dataToReturn = canvasData['data']
     response = { 'returnData' : dataToReturn }
+    print("response") 
     print(response) 
     return response
 
-# Create and save csv of image data
-def create_csv(text):
-    unique_id = str(uuid.uuid4())
-    with open('images/'+unique_id+'.csv', 'a') as file:
-        file.write(text[1:-1]+"\n")
-    return unique_id
+
+# # Create and save csv of image data
+# def create_img(text):
+#     unique_id = str(uuid.uuid4())
+#     with open('images/'+unique_id+'.csv', 'a') as file:
+#         file.write(text[1:-1]+"\n")
+#     return unique_id
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5050, debug=True)
